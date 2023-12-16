4b415804ed676c635e84626c0cb34bb98262f71f
m-sepehr <m-sepehr@hotmail.com>
2023-12-16 04:07:43 -0500

    adding opcode functionality, implementing get and put with opcode

--- client/client.py
+++ client/client.py
@@ -1,8 +1,31 @@
 import os
 import sys
 import socket
+import struct
 
 bufferSize = 1024
+
+def create_request(opcode, filename):
+
+    opcode_and_length = (opcode << 5) + len(filename)
+    filename = filename.encode('utf-8')
+    
+
+    if opcode == 0:
+        file_size = os.path.getsize(filename)
+        request = struct.pack('B', opcode_and_length) + filename + struct.pack('I', file_size)
+    elif opcode == 1:
+        request = struct.pack('B', opcode_and_length) + filename
+
+    return request
+
+def create_request_change_name(opcode, filename, new_filename):
+    opcode_and_length = (opcode << 5) + len(filename)
+    filename = filename.encode('utf-8')
+    new_filename = new_filename.encode('utf-8')
+    request = struct.pack('B', opcode_and_length) + filename + new_filename
+    return request
+
 
 def main():
     first_start = True
@@ -13,6 +36,10 @@ def main():
         if (first_start):
             cli_text = "myftp> "
             first_start = False
+        
+        #------------------------------------------------------------
+        # TCP or UDP
+        #------------------------------------------------------------
         if command == "1" or command == "2":
             address = input("myftp> Provide IP address and Port number separated by a space: ").split()
             localIP = address[0]
@@ -34,16 +61,54 @@ def main():
                 UDPServerSocket.connect((localIP, localPort))
                 print("myftp> Session has been established using UDP.")
                 conn_type = "UDP"
-                
+
+        #------------------------------------------------------------
+        # GET
+        #------------------------------------------------------------ 
         elif command.startswith("get"):
-            # Extract filename and call get_file
-            pass
+            filename = command.split()[1]
+            opcode = 1 # 001 for get
+
+            request = create_request(opcode, filename)
+            print(request)
+
+            # ~~~~~~~~~~~~~~~~~~~~~
+            # UDP
+            # ~~~~~~~~~~~~~~~~~~~~~
+            if conn_type == "UDP":
+                # receive file over UDP
+                UDPServerSocket.send(request)
+                with open(filename, 'wb') as f:
+                    while True:
+                        data, addr = UDPServerSocket.recvfrom(bufferSize)
+                        if data == b'END':
+                            print("File transmission completed")
+                            break
+                        f.write(data)
+            
+            # ~~~~~~~~~~~~~~~~~~~~~
+            # TCP
+            # ~~~~~~~~~~~~~~~~~~~~~
+            
+
+
+        #------------------------------------------------------------
+        # PUT
+        #------------------------------------------------------------
         elif command.startswith("put"):
             filename = command.split()[1]
+            opcode = 0 # 000 for put
+            
 
+            request = create_request(opcode, filename)
+            print(request)
+
+            # ~~~~~~~~~~~~~~~~~~~~~
+            # UDP
+            # ~~~~~~~~~~~~~~~~~~~~~
             if conn_type == "UDP":
                 # Send file over UDP
-                UDPServerSocket.send(filename.encode())
+                UDPServerSocket.send(request)
 
                 with open(filename, 'rb') as f:
                     while True:
@@ -54,7 +119,9 @@ def main():
                 # Send end-of-transmission signal
                 UDPServerSocket.send(b'END')
                 
-
+            # ~~~~~~~~~~~~~~~~~~~~~
+            # TCP
+            # ~~~~~~~~~~~~~~~~~~~~~
             elif conn_type == "TCP":
                 # Send file over TCP
                 TCPServerSocket.send(filename.encode())
@@ -67,10 +134,14 @@ def main():
                         TCPServerSocket.send(data)
                 TCPServerSocket.send(b'END')
                 
-
+        #------------------------------------------------------------
+        # CHANGE FILENAME
+        #------------------------------------------------------------   
         elif command.startswith("change"):
-            # Extract filenames and call change_file
-            pass
+            filename = command.split()[1]
+            new_filename = command.split()[2]
+            opcode = 2 # 010 for change
+            request = create_request_change_name(opcode, filename, new_filename)
         elif command == "help":
             # he
             pass
